8d1216f7f9ecc5533ba18d8eafc9d152556f84e1
YaroslavVFKFIRJ <150508580+YaroslavVFKFIRJ@users.noreply.github.com>
2024-03-22 11:35:20 +0300

    Add files via upload

---
+++ js/burger-menu.js
@@ -0,0 +1,9 @@
+'use strict';
+document.querySelector(".burger-but").onclick=( )=> {
+    background.classList.add("visback");
+    document.querySelector(".burger-menu").classList.add("visback");
+}
+document.querySelector(".burger-close").onclick=( )=> {
+    background.classList.remove("visback");
+    document.querySelector(".burger-menu").classList.remove("visback");
+}
